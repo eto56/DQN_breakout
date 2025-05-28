2dfe5ec068b28111014cfd0c2a12d8bc8f29125d
eto56 <arimakishou0505@gmail.com>
2025-05-28 14:11:55 +0900

    Ignore run.sh

--- script/run_example.sh
+++ script/run_example.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-#SBATCH -p dgx-a100-80g    
-#SBATCH -G 1    
-#SBATCH -t 3-0  
-#SBATCH -J run
-#SBATCH -o ../log/stdout.%J   
-#SBATCH -e ../log/stderr.%J 
-#SBATCH --mail-type=ALL          # when you want to get notifications. You can select one from [BEGIN , END , FAIL , REQUEUE , ALL] 
-#SBATCH --mail-user="matsukawa@mi.t.u-tokyo.ac.jp" # Email address which receives notifications
-
 model_path="model.pth"
 
 max_step=10000000
